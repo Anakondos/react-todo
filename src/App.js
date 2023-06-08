ffe76df8706532e0fa189a0082a6be63a61fb9a1
Anakondos <anakondos@gmail.com>
2023-06-07 23:11:12 -0400

    map todoList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: "Complete assigment" 
+  },
+  {
+    id: 2, 
+    title: "submit assigment"
+  },
+  {
+    id: 3,
+    title: "Start assignment"
+  }
+];
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Todo list</h1>
+      <ul>
+          {
+            todoList.map(function(item){
+              return <li key = {item.id}>
+                {item.title}
+                </li>
+            })
+          }
+      </ul>
     </div>
   );
 }
