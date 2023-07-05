2b6cd331d7f6e1f1bbbf1cc21f05b67d7246b8ed
Anakondos <anakondos@gmail.com>
2023-07-05 19:00:15 -0400

    Move Todo List into State

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import AddTodoForm from './AddTodoForm';
 
 
 function App() {
+
  
     const [newTodo, setNewTodo] = React.useState('');
  
-
+    /*1.4 new state variable named todoList 
+    with setter setTodoList and default value of an empty Array*/
+    const [todoList, setTodoList] = React.useState([]);
+    
+    // const handleSearch = (event) => {
+    //   setTodoList(event.target.value);
+    // }
+    
   return (
     <div>
       <h1>Todo list</h1>
       <AddTodoForm onAddTodo={setNewTodo}/>
         <p>Value of NewTodo = {newTodo}</p>
-      <TodoList />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,25 +2,26 @@ import React from 'react';
 
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-    {
-      id: 1,
-      title: "Complete assigment" 
-    },
-    {
-      id: 2, 
-      title: "submit assigment"
-    },
-    {
-      id: 3,
-      title: "Start assignment"
-    }
-  ];
+// const todoList = [
+//     {
+//       id: 1,
+//       title: "Complete assigment" 
+//     },
+//     {
+//       id: 2, 
+//       title: "submit assigment"
+//     },
+//     {
+//       id: 3,
+//       title: "Start assignment"
+//     }
+//   ];
 const TodoList = (props) => {
-
+ 
     return (
         <ul>
-          {todoList.map(function(item){
+          
+          {props.todoList.map(function(item){
               return (
                 <TodoListItem key={item.id} item={item} />
               );
