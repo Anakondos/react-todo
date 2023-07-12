c7e59789f42a30ecf096b498873237f73ce68561
Anakondos <anakondos@gmail.com>
2023-07-12 00:19:02 -0400

    Create Custom Hook

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+function useSemiPersistentState() {
+  
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList'))|| []
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList]
+}
+
 
 function App() {
 
  //   create state for App with blank Array and 'todoList' variable and setter 'setTodoList'
-    const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList'))|| []
-    );
     
+ const [todoList, setTodoList] = useSemiPersistentState();
 
-    React.useEffect(() => {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-    }, [todoList]);
+    
   
  //   update STATE function 
     const addTodo = (newTodo) => {
