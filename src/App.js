5037840cdc807404b7dea24a89164a91ddccf2dc
Anakondos <anakondos@gmail.com>
2023-07-26 18:59:17 -0400

    Async mimic

--- src/App.js
+++ src/App.js
@@ -2,32 +2,33 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-// function useSemiPersistentState() {
-  
-//   const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList'))|| []
-//   );
-
-//   React.useEffect(() => {
-//     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-//   }, [todoList]);
-
-//   return [todoList, setTodoList]
-// }
-
-
 
 function App() {
 
  //   create state for App with blank Array and 'todoList' variable and setter 'setTodoList'
+
+    const [todoList, setTodoList] = React.useState([]);
+
+
+//--handler function--
+  const getAsyncTodoList = () => 
+
+  new Promise((resolve) => 
+    setTimeout(
+      () => resolve({data: {todoList: todoList} }), 
+        2000
+        )
     
-//  const [todoList, setTodoList] = useSemiPersistentState();
+  ).then((result)=>{setTodoList(result.data.todoList)});
 
-      const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList'))|| []
-  );
 
-  React.useEffect(() => {
+
+
+    React.useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList]);
+
+
   
  //   update STATE function 
     const addTodo = (newTodo) => {
